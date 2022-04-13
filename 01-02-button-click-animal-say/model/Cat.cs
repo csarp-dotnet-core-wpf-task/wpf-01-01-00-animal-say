c01c4ce658918d09d8a61cc34c9c18de27b672f3
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-04-13 17:45:05 +0200

    A kész feladat

---
+++ 01-02-button-click-animal-say/model/Cat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_02_button_click_animal_say.model
+{
+    /// <summary>
+    /// S2.01 Macska osztály - a macska köszinti gazdáját
+    /// </summary>
+    class Cat
+    {
+
+        /// <summary>
+        /// A macska neve
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// A macska így köszönti gazdáját
+        /// </summary>
+        private string greet;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="name">A macska neve</param>
+        public Cat(string name)
+        {
+            this.name = name;
+            this.greet = "miaú-miaú";
+        }
+
+        /// <summary>
+        /// A macska köszöntése stringként
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            // S2.10 Javítjuk az osztályt (refactoring). Magyarul köszön. Adattagok private.
+            string output = name + " macska mondja: " + greet+".";
+            return output;
+        }
+    }
+}
---
+++ 01-02-button-click-animal-say/model/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_02_button_click_animal_say.model
+{
+    // <summary>
+    /// S2.01 Kutya - a kutya köszinti gazdáját
+    /// </summary>
+    class Dog
+    {
+
+        /// <summary>
+        /// A kutya neve
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// A kutya így köszönti gazdáját
+        /// </summary>
+        private string greet;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="name">A kutya neve</param>
+        public Dog(string name)
+        {
+            this.name = name;
+            this.greet = "vau-vau";
+        }
+
+        /// <summary>
+        /// A kutya köszöntése stringként
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string output = name + " kutya mondja: " + greet + ".";
+            return output;
+        }
+    }
+}
